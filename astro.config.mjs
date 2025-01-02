de52aef9400aac3e7aa940f157563263de082069
Erik Wright <hey@erikwright.dev>
2025-01-02 20:11:56 +0100

    feat: add semantic release

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
-
-  adapter: node({
-    mode: "standalone",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
   }),
 });
